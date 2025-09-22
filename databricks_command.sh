97af2800343d939c1719a1c9707b13e14d0819bc
nyu9hc <nguyenhoangthien1998@gmail.com>
2025-09-22 11:18:15 +0700

    Updating command to authenticate the shell

--- databricks_command.sh
+++ databricks_command.sh
@@ -1,5 +1,11 @@
 # HOST
 databricks configure --host <HOST_ADDRESS>
+
+# Setup shelll
+$env:DATABRICKS_HOST=<Host>
+$env:DATABRICKS_TOKEN=<Token>
+$env:DATABRICKS_AUTH_TYPE='pat' # if needed
+
 # Create Clusters
 databricks clusters create --json @create-compute.json --profile DEFAULT
 
@@ -18,6 +24,7 @@ databricks bundle validate
 
 # Deploying
 databricks bundle deploy
+databricks bundle deploy -t <TARGET> --profile <AUTH_PROFILE> --var catalog=citibike_test --var existing_cluster_id=0709-053523-uls3spa0 --var dev_flag=false
 
 # Destroy
 databricks bundle destroy -t <TARGET>
